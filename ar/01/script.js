275d0f063720d3d41626ffed6fe9b6e8c0bf5045
mainkai <mainkai@users.noreply.github.com>
2021-05-22 00:15:38 +0200

    add js with 2 wind turbines

---
+++ ar/01/script.js
@@ -0,0 +1,45 @@
+window.onload = () => {
+     let places = staticLoadPlaces();
+     renderPlaces(places);
+};
+
+function staticLoadPlaces() {
+    return [
+        {
+            name: 'wind_hg',
+            location: {
+                lat: 48.607377,
+                lng: 8.201714,
+            }
+        },
+        {
+            name: 'wind_home',
+            location: {
+                lat: 48.628049,
+                lng: 8.082099,
+            }
+        },
+    ];
+}
+
+function renderPlaces(places) {
+    let scene = document.querySelector('a-scene');
+
+    places.forEach((place) => {
+        let latitude = place.location.lat;
+        let longitude = place.location.lng;
+
+        let model = document.createElement('a-entity');
+        model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+        model.setAttribute('gltf-model', './assets/wind_turbine_01/windturbine.gltf');
+        model.setAttribute('rotation', '0 180 0');
+        model.setAttribute('animation-mixer', '');
+        model.setAttribute('scale', '0.5 0.5 0.5');
+
+        model.addEventListener('loaded', () => {
+            window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
+        });
+
+        scene.appendChild(model);
+    });
+}

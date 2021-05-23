86f3bd9e0484c1813d36e8e9f1ab59405dda249e
mainkai <mainkai@users.noreply.github.com>
2021-05-24 01:59:56 +0200

    Update script.js

--- ar/01/script.js
+++ ar/01/script.js
@@ -11,6 +11,7 @@ function staticLoadPlaces() {
                json[i].name = "Wind " + i;
                json[i].location.lat = json[i].geometry.coordinates[1];
                json[i].location.lon = json[i].geometry.coordinates[0];
+               json[i].location.lon = json[i].geometry.coordinates[0];
           }
           return json
      });
@@ -38,18 +39,32 @@ function renderPlaces(places) {
     places.forEach((place) => {
         let latitude = place.location.lat;
         let longitude = place.location.lng;
+        let local_height = place.local_height;
 
         let model = document.createElement('a-entity');
         model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-        model.setAttribute('gltf-model', './assets/wind_turbine_01/windturbine.gltf');
+        model.setAttribute('gltf-model', '../02/assets/turbine/scene.gltf');
+        model.setAttribute('position', `0 ${local_height} 0`);
         model.setAttribute('rotation', '0 180 0');
         model.setAttribute('animation-mixer', '');
-        model.setAttribute('scale', '0.5 0.5 0.5');
+        model.setAttribute('scale', '30 30 30');
 
         model.addEventListener('loaded', () => {
             window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
         });
 
         scene.appendChild(model);
+         
+         
+        let name = place.name;
+        let upper_pos = place.local_height + place.hub_height_m;
+         
+        let desc = document.createElement('a-text');
+        desc.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+        desc.setAttribute('value', `0 ${name} 0`);
+        desc.setAttribute('position', `0 ${upper_pos} 0`);
+        desc.setAttribute('look-at', "[gps-camera]");
+        desc.setAttribute('scale', '20 20 20');
+        scene.appendChild(desc);
     });
 }

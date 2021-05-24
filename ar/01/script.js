994bf956ff2353d396c08649881db42e2e43d3d7
mainkai <mainkai@users.noreply.github.com>
2021-05-24 21:48:43 +0200

    Update script.js

--- ar/01/script.js
+++ ar/01/script.js
@@ -27,15 +27,14 @@ function load_turbines_from_json(json) {
 	console.log(json);
 	let scene = document.querySelector('a-scene');
 	for (var i = 0; i < json.length; i++) {
-		// add wind turbine 3d model
+		// add wind turbine 3D model
 		let latitude = json[i].geometry.coordinates[1];
 		let longitude = json[i].geometry.coordinates[0];
 		let local_height = json[i].properties.local_height;
-		let gltf_file = "../02/assets/turbine/scene.gltf";
 
 		let model = document.createElement('a-entity');
 		model.setAttribute('gps-entity-place', "latitude: " + latitude + "; longitude: " + longitude + ";");
-		model.setAttribute('gltf-model', `${gltf_file}`);
+		model.setAttribute('gltf-model', '../02/assets/turbine/scene.gltf');
 		model.setAttribute('position', {
 			x: 0,
 			y: local_height, 
@@ -50,16 +49,13 @@ function load_turbines_from_json(json) {
 		scene.appendChild(model);
 
 		// add descriptions text for turbine
-		let name = "Wind " + i;
-		let hub_height_m = json[i].properties.hub_height_m;
-		let upper_pos = local_height + hub_height_m;
 		let desc = document.createElement('a-text');
 		desc.setAttribute('gps-entity-place', `latitude: ${ latitude }; longitude: ${ longitude };`);
 		desc.setAttribute('value', "Wind " + i);
-		desc.setAttribute('position', `0 "${upper_pos}" 0`);
+		desc.setAttribute('position', `0 "${local_height + json[i].properties.hub_height_m}" 0`);
 		desc.setAttribute('look-at', "[gps-camera]");
 		desc.setAttribute('scale', '20 20 20');
 		scene.appendChild(desc);
-		console.log("added: " + name + " at: " + latitude + ", " + longitude + ", " + local_height + " m, " + hub_height_m + " m");
+		console.log("added: " + name + " at: " + latitude + ", " + longitude + ", " + local_height + " m, " + json[i].properties.hub_height_m + " m");
 	}
 }

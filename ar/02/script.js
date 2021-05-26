77d17595403614947530f34409eaa69fbb9e3658
mainkai <mainkai@users.noreply.github.com>
2021-05-26 11:14:00 +0200

    Update script.js

--- ar/02/script.js
+++ ar/02/script.js
@@ -57,8 +57,8 @@ function load_turbines_from_json(json) {
 		
 		// add line from turbine to text
 		const line = document.createElement('a-entity');
-		line.setAttribute('line', `start: 0 ${hub_height_m} 0; end: 0 ${total_turbine_size + 2} 0; color: gray`);
-		line.setAttribute('line__1', `start: 0 ${total_turbine_size + 2} 0; end: ${rotor_diameter_m} ${total_turbine_size + 2} 0; color: blue`);
+		line.setAttribute('line', `start: 0 ${hub_height_m} 0; end: ${rotor_diameter_m} ${total_turbine_size + 2} 0; color: gray`);
+		line.setAttribute('line__1', `start: ${rotor_diameter_m} ${total_turbine_size + 2} 0; end: ${2 * rotor_diameter_m} ${total_turbine_size + 2} 0; color: blue`);
 		line.setAttribute('look-at', "[gps-projected-camera]");
 		model.appendChild(line);
 		

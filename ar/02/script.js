9b20743afa254c0523a61e6fc20bfa09544dd9eb
mainkai <mainkai@users.noreply.github.com>
2021-05-27 00:32:56 +0200

    Update script.js

--- ar/02/script.js
+++ ar/02/script.js
@@ -1,6 +1,6 @@
 window.onload = () => {
 	load_turbines_json();
-	load_track_gpx();
+	//load_track_gpx();
 };
 
 window.addEventListener('gps-projected-camera-update-position', e => {
@@ -9,7 +9,7 @@ window.addEventListener('gps-projected-camera-update-position', e => {
         });
 
 function load_turbines_json() {
-	fetch("../assets/wind_potentials/Achern/placed_turbines.geojson")
+	fetch("../assets/wind_potentials/Freiburg/placed_turbines.geojson")
 	  .then(response => response.json())
 	  .then(json => load_turbines_from_json(json));
 }

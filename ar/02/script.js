a6b66a2f9b24a1a92a277a4929cf2d0d179fd64c
mainkai <mainkai@users.noreply.github.com>
2021-05-26 21:56:32 +0200

    Update script.js

--- ar/02/script.js
+++ ar/02/script.js
@@ -87,11 +87,12 @@ function add_track(text) {
 			lat = track_point.getAttribute("lat");
 			lon = track_point.getAttribute("lon");
 			let ele = track_point.getElementsByTagName("ele")[0].innerHTML;
+			let ele_up = ele + 25;
 			console.log(`segment ${i}, track_point ${j}: (${lon}, ${lat}), ${ele} m`);
 			
 			const line_ = document.createElement('a-entity');
 			line_.setAttribute('gps-projected-entity-place', `latitude: ${lat}; longitude: ${lon};`);
-			line_.setAttribute('line', `start: 0 ${ele} 0; end: 0 ${ (ele+25) } 0; color: green`);
+			line_.setAttribute('line', `start: 0 ${ele} 0; end: 0 ${ele_up} 0; color: green`);
 			scene.appendChild(line_);
 		}
 	}

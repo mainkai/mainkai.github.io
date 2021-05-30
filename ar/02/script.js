fbd50ead743a70e459c0e3986b1a58ca665adc5e
mainkai <mainkai@users.noreply.github.com>
2021-05-30 12:41:10 +0200

    Update script.js

--- ar/02/script.js
+++ ar/02/script.js
@@ -1,8 +1,6 @@
 window.onload = () => {
-	console.log(document.getElementById('fov'));
 	document.getElementById('fov').innerHTML = "Paragraph changed!";
 	console.log(document.getElementById('fov').innerHTML);
-	console.log(document.getElementById('fov'));
 	load_turbines_json();
 	//load_track_gpx();
 	
